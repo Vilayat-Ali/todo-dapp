5a37836f9718f6d18962d1cbfeb05e83e64457a1
Syed Vilayat Ali Rizvi <vilayatcodemysite@gmail.com>
2022-08-08 21:48:47 +0530

    Added web3 functionality and styles

--- src/pages/Account/index.jsx
+++ src/pages/Account/index.jsx
@@ -1,6 +1,11 @@
 // Libraries
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// import UI
+import { Button, Box, Text, Tag, Badge } from "@chakra-ui/react";
 
 const Account = () => {
   // state
@@ -9,10 +14,110 @@ const Account = () => {
   const [chainId, setChainID] = useState(undefined);
   const [chainAddress, setChainAddress] = useState(undefined);
 
-  // useEffect
-  useEffect(() => {}, []);
+  // toast
+  const notify = () => toast("Wallet Address Copied !!!");
 
-  return <div>Account</div>;
+  // fetch data
+  const fetchWalletData = async () => {
+    /// web3 provider
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    // signing
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    const balance = await signer.getBalance();
+    const chainID = await signer.getChainId();
+    setAddress(address);
+    setBalance(ethers.utils.formatEther(balance));
+    setChainID(chainID);
+  };
+
+  const copyToClipBoard = async (data) => {
+    await navigator.clipboard.writeText(data);
+  };
+
+  return (
+    <>
+      <Box
+        display="flex"
+        flexDirection="Column"
+        alignItems="center"
+        justifyContent="flex-start"
+        sx={{ width: "100%", height: "85vh" }}
+      >
+        {!address ? (
+          <Box
+            sx={{
+              width: { sm: "95%", xs: "95%", md: "60%" },
+              margin: "auto auto",
+            }}
+            className="shadow p-8"
+          >
+            <Button onClick={fetchWalletData}>Connect Wallet</Button>
+          </Box>
+        ) : (
+          <>
+            <ToastContainer />
+            <Box
+              sx={{
+                width: { sm: "95%", xs: "95%", md: "60%" },
+                margin: "auto auto",
+              }}
+              className="shadow p-8"
+            >
+              <Text>
+                <Tag
+                  size={{ sm: "sm", xs: "xs", md: "md" }}
+                  variant="solid"
+                  className="mr-5"
+                  bgColor={"gray.500"}
+                >
+                  Wallet Address
+                </Tag>
+                {address}
+                <Badge
+                  className="ml-5 cursor-pointer"
+                  bgColor="green.200"
+                  onClick={() => {
+                    copyToClipBoard(address);
+                    notify();
+                  }}
+                >
+                  Copy
+                </Badge>
+              </Text>
+
+              {/* Balance */}
+              <Text className="mt-4">
+                <Tag
+                  size={{ sm: "sm", xs: "xs", md: "md" }}
+                  variant="solid"
+                  className="mr-5"
+                  bgColor={"green.500"}
+                >
+                  Wallet Balance
+                </Tag>
+                {balance} <Badge>ETH</Badge>
+              </Text>
+
+              {/* Chain ID */}
+              <Text className="mt-4">
+                <Tag
+                  size={{ sm: "sm", xs: "xs", md: "md" }}
+                  variant="solid"
+                  className="mr-5"
+                  bgColor={"yellow.500"}
+                >
+                  Chain ID
+                </Tag>
+                {chainId}
+              </Text>
+            </Box>
+          </>
+        )}
+      </Box>
+    </>
+  );
 };
 
 export default Account;
--- src/pages/Todo/index.jsx
+++ src/pages/Todo/index.jsx
@@ -1,7 +1,75 @@
 // Libraries
+import { useEffect, useState, useRef } from "react";
+
+// UI
+import { Box, VStack, HStack, Text, Input, Button } from "@chakra-ui/react";
+
+const NewToDoInput = () => {
+  // state
+  const [todo, setTodo] = useState();
+  const inputRef = useRef(null);
+  // save function
+  const saveTodo = () => {
+    console.log(todo);
+  };
+  return (
+    <Box className="my-12">
+      <HStack>
+        <Input
+          placeholder="Enter a new task"
+          onChange={(e) => {
+            e.preventDefault();
+            setTodo(e.target.value);
+          }}
+          ref={inputRef}
+        />
+        {todo === "" ? null : (
+          <Button
+            color="white"
+            bgColor="red.600"
+            onClick={() => {
+              inputRef.current.value = "";
+              setTodo("");
+            }}
+          >
+            Clear
+          </Button>
+        )}
+
+        <Button color="white" bgColor="green.600" onClick={saveTodo}>
+          Save
+        </Button>
+      </HStack>
+    </Box>
+  );
+};
 
 const Todo = () => {
-  return <div>Todo</div>;
+  // state
+  const [todos, setTodos] = useState([""]);
+
+  // useEffect
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+      });
+  }, []);
+  // return
+  return (
+    <Box>
+      <VStack>
+        <NewToDoInput />
+        <h2>Todo Count: {todos.length}</h2>
+        {todos.map((todo) => (
+          <Box className="mt-2 shadow" sx={{ width: "80%" }} key={todo.id}>
+            <Text className="text-center py-5 text-gray-500">{todo.title}</Text>
+          </Box>
+        ))}
+      </VStack>
+    </Box>
+  );
 };
 
 export default Todo;
